d979095426b9992020ef874f09f837f0972fe07a
TheBlackParrot <theblackparrot0@gmail.com>
2025-04-12 06:11:11 -0500

    initial commit

---
+++ BetterSongList_RandomSort/Classes/RandomSorter.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BetterSongList.Interfaces;
+using BetterSongList.SortModels;
+using UnityEngine;
+
+namespace BetterSongList_RandomSort.Classes;
+
+internal class RandomSorter : ISorterPrimitive, ITransformerPlugin
+{
+    public string name => "Random";
+    public bool visible => true;
+    public bool isReady => true;
+
+    public Task Prepare(CancellationToken cancelToken) => Task.CompletedTask;
+    
+#if V1_29_1
+    public float? GetValueFor(IPreviewBeatmapLevel level)
+#else
+    public float? GetValueFor(BeatmapLevel level)
+#endif
+    {
+        return Random.Range(float.MinValue, float.MaxValue);
+    }
+
+#if V1_29_1
+    public void ContextSwitch(SelectLevelCategoryViewController.LevelCategory levelCategory, IAnnotatedBeatmapLevelCollection? playlist)
+#else
+    public void ContextSwitch(SelectLevelCategoryViewController.LevelCategory levelCategory, BeatmapLevelPack? playlist)
+#endif
+    {
+        // do nothing
+    }
+}
---
+++ BetterSongList_RandomSort/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BetterSongList_RandomSort.Classes;
+using BetterSongList;
+using IPA;
+using JetBrains.Annotations;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BetterSongList_RandomSort;
+
+[Plugin(RuntimeOptions.SingleStartInit)]
+[NoEnableDisable]
+[UsedImplicitly]
+internal class Plugin
+{
+    private static RandomSorter RandomSorterInstance => new();
+    private static IPALogger Log { get; set; } = null!;
+
+    [Init]
+    public Plugin(IPALogger logger)
+    {
+        Log = logger;
+        
+        SortMethods.RegisterPrimitiveSorter(RandomSorterInstance);
+        Log.Info("Plugin loaded");
+    }
+}
